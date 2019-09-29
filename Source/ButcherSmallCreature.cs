2435a9f9a9c12c4b0dba78412f4b3274261132bd
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2019-09-28 23:46:17 -0600

    replaced meatDef check with ShouldShowFor() for StatDefOf.MeatAmount

--- Source/ButcherSmallCreature.cs
+++ Source/ButcherSmallCreature.cs
@@ -9,9 +9,11 @@ namespace AssortedAlterations
         {
             public static void DefsLoaded() {
                 var smallCorpses = ThingCategoryDefOf.Corpses.ThisAndChildCategoryDefs.SelectMany(x => x.childThingDefs).Where(
-                    x => x.ingestible?.sourceDef?.race?.meatDef != null && 
-                         x.ingestible.sourceDef.GetStatValueAbstract(StatDefOf.MeatAmount) > 0 &&
-                         x.ingestible.sourceDef.GetStatValueAbstract(StatDefOf.MeatAmount) <= 75);
+                    x =>
+                        x.ingestible?.sourceDef != null &&
+                        StatDefOf.MeatAmount.Worker.ShouldShowFor(StatRequest.For(x.ingestible.sourceDef, null)) &&
+                        x.ingestible.sourceDef.GetStatValueAbstract(StatDefOf.MeatAmount) > 0 &&
+                        x.ingestible.sourceDef.GetStatValueAbstract(StatDefOf.MeatAmount) <= 75);
                 // second parameter is "exceptedFilters" but it's actually the filters to disallow apparently??
                 ConvenientButcherRecipes.COAA_ButcherSmallCreatureFlesh.fixedIngredientFilter.SetDisallowAll(smallCorpses, new[] {SpecialFilter.AllowRotten});
             }
